afbb852bef4e0b21457ce7f81f3c31be780c1e79
Yolamufrancis <43439513+Yolamufrancis@users.noreply.github.com>
2024-09-13 16:31:05 +0100

    Fixed BAse Url

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/backend': {
-        target: 'http://localhost:3000',
-        secure: false,
-      }
-    }
+      // Proxy all requests starting with '/api/' to the backend API
+      '/api/': {
+        target: 'https://api.smartvillageshub.com/api/',
+        changeOrigin: true, // Required for CORS handling
+        secure: false, // Allow insecure connections for development
+      },
+    },
   },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
