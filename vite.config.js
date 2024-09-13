da4576b18b70b8f3e208ca00c8da1b7d22ffa146
Yolamufrancis <43439513+Yolamufrancis@users.noreply.github.com>
2024-09-13 16:35:37 +0100

    Fixed Request Proxy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       // Proxy all requests starting with '/api/' to the backend API
-      '/api/': {
+      '/backend/': {
         target: 'https://api.smartvillageshub.com/api/',
         changeOrigin: true, // Required for CORS handling
         secure: false, // Allow insecure connections for development
