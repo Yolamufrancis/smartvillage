38cacc4056ae1dfb55e4d333f93ff532a811c2bc
Milton Chithenga <miltonachithenga@gmail.com>
2024-09-17 12:25:53 +0200

    Fixed target

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      // Proxy all requests starting with '/api/' to the backend API
-      '/backend/': {
+      '/backend': {
         target: 'https://api.smartvillageshub.com/api/',
-        changeOrigin: true, // Required for CORS handling
-        secure: false, // Allow insecure connections for development
-      },
-    },
+        secure: false,
+      }
+    }
   },
-  plugins: [react()],
-});
+  plugins: [react()]
+})
