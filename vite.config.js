0306c7ee6ed69150dbcc771e8258bbc7f6d1510c
Yolamufrancis <43439513+Yolamufrancis@users.noreply.github.com>
2024-09-24 11:39:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/backend': {
+      'https://smartvillageshub.com/backend': {
         target: 'https://api.smartvillageshub.com/api/',
         secure: false,
       }
